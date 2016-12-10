814ecfe90e6c9ce89ec931364f968bcb49a7c758
ngochai117 <congtuhot9.9@gmail.com>
2016-12-10 09:45:44 +0700

    khóa học đang chờ

---
+++ web/WebContent/controller/CourseWaitingServlet.java
@@ -0,0 +1,107 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.CourseWaiting;
+import model.User_info;
+import model.Course;
+import dao.CourseWaitingDAO;
+
+
+/**
+ * Servlet implementation class CourseWaitingServlet
+ */
+@WebServlet("/CourseWaitingServlet")
+public class CourseWaitingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CourseWaitingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		String command = request.getParameter("command");
+		String url = "";
+		CourseWaiting coursewaiting = new CourseWaiting();
+		CourseWaitingDAO coursewaitingDAO = new CourseWaitingDAO();
+		
+		RequestDispatcher rd ;
+		
+		HttpSession session = request.getSession();
+		User_info user_register= (User_info)session.getAttribute("user_info");
+		Course course_register = (Course)session.getAttribute("course_register");
+		
+		switch(command){
+		case "insert":
+			long x = new java.util.Date().getTime();
+			coursewaiting.setCourse_waiting_id(x);
+			coursewaiting.setUser_id(user_register.getId());
+			coursewaiting.setCourse_id(course_register.getCourse_id());			
+			coursewaiting.setTime_register(new Timestamp(new Date().getTime()));	
+			
+			
+			boolean f = coursewaitingDAO.insert(coursewaiting);
+			if(f)
+			{
+				response.getWriter().write("Đăng ký khóa học thành công. Vui lòng chờ xác nhận !");
+			}
+			else
+			{
+				//session.removeAttribute("user");
+				response.getWriter().write("Đăng ký thất bại !");
+			}
+			/*RequestDispatcher rd = request.getRequestDispatcher(url);
+			rd.forward(request, response);*/
+			break;	
+		case "delete":
+			url="";
+			
+			String course_waiting_id = request.getParameter("course_waiting_id");
+			String course_id = request.getParameter("course_id");
+			f = coursewaitingDAO.delete(Long.parseLong(course_waiting_id));
+			if(f)
+				url="khoahoc2.jsp?course_id="+course_id;
+		//	else
+		//		url="edit-section.jsp?course_id="+course_id+"&course_waiting_id="+course_waiting_id;
+			rd = request.getRequestDispatcher(url);
+			rd.forward(request, response);
+			break;
+			
+		}
+		}
+		
+		
+	}
+
+}
---
+++ web/WebContent/dao/CourseWaitingDAO.java
@@ -0,0 +1,121 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connect.DBConnect;
+import model.Course;
+import model.CourseWaiting;
+import model.User_info;;
+
+public class CourseWaitingDAO {
+
+	private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PreparedStatement ps;
+	}
+	
+	public boolean insert(CourseWaiting cw)
+	{
+		Connection con = DBConnect.getConnecttion();
+		String sql = "insert into message values(?,?,?,?)";
+		PreparedStatement ps;
+		try {
+			ps = (PreparedStatement) con.prepareCall(sql);
+			ps.setLong(1, cw.getCourse_waiting_id());
+			ps.setLong(2, cw.getUser_id());
+			ps.setLong(3, cw.getCourse_id());
+			ps.setTimestamp(4, cw.getTime_register());
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean delete(long course_waiting_id)
+	{
+		Connection con = DBConnect.getConnecttion();
+		String sql = "delete from course_waiting where course_waiting_id=?";
+		PreparedStatement ps;
+		try {
+			ps = (PreparedStatement) con.prepareCall(sql);
+			ps.setLong(1, course_waiting_id);
+			ps.executeUpdate();
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	//Lấy teacher
+	public User_info getteacher(long course_id) {
+        try {
+        	Connection conn = DBConnect.getConnecttion();
+            String sql = "select * from user_info, course_user where user_info.id = course_user.id and quyen=1 and course_id='"+course_id+"'";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            User_info u = new User_info();
+            while (rs.next()) {
+            	u.setId(rs.getLong(1));
+            	u.setTen(rs.getString(2));
+            	u.setSodienthoai(rs.getString(3));
+            	u.setGioitinh(rs.getInt(4));
+            	u.setNgaysinh(rs.getString(5));
+            	u.setEmail(rs.getString(6));
+            	
+            	u.setDiachi(rs.getString(8));
+            	u.setQuyen(rs.getInt(9));            	
+            }
+            conn.close();
+            return u;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+	//Lấy danh sách khóa học đang chờ xác nhận theo từng học viên
+	public List<Course> getListCourseWaiting(long user_id) {
+        try {
+        	Connection conn = DBConnect.getConnecttion();
+            String sql = "select * from course_waiting, course where course_waiting.course_id = course.course_id and user_id='"+user_id+"'";
+            
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+             
+            List<Course> listCourseWaiting = new ArrayList<Course>();
+            
+            while (rs.next()) {
+            	Course cw = new Course();
+            	
+            	cw.setCourse_id(rs.getLong(3));
+            	cw.setCourse_name(rs.getString(6));
+            	cw.setCourse_startdate(rs.getString(7));
+            	cw.setCourse_enddate(rs.getString(8));
+            	cw.setCourse_schedulingday(rs.getInt(9));
+            	cw.setCourse_startlession(rs.getInt(10));
+            	cw.setCourse_endlession(rs.getInt(11));
+            	cw.setCourse_place(rs.getString(12));
+            	cw.setCourse_description(rs.getString(13));
+            	
+            	listCourseWaiting.add(cw);
+            }
+            conn.close();
+            return listCourseWaiting;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
